0b8b8c8293ddbaa34f8d7187f1a23b04b101287d
amaranthineblu <amaranthineblu@yahoo.com>
2023-07-29 08:13:25 +0100

    add

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use App\Models\Exam;
 use App\Models\Option;
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -18,11 +19,12 @@ class DashboardController extends Controller
     }
 
     function allTest() {
+        $user = Auth::user()->id;
         $exams = Exam::latest()->get();
         foreach ($exams as $exam) {
             $exam_id = $exam->pluck('id');
             $questions = Question::where('exam_id',$exam_id)->get();
-            foreach ($questions as $question) {
+            foreach (Question::all() as $question) {
                 $question_id = $question->pluck('id');
                 $option = Option::where('question_id', $question_id)->get();
                 # code...
@@ -31,8 +33,9 @@ class DashboardController extends Controller
         // dd($question_id);
 
         return view('user.all-test', with([
-            'exam' => $exam,
-            'exam_id' => $exam,
+            'user' => $user,
+            'exams' => $exams,
+            'exam_id' => $exam_id,
             'questions' => $questions,
             'question_id' => $question_id,
             'option' => $option
--- resources/views/user/all-test.blade.php
+++ resources/views/user/all-test.blade.php
@@ -1,38 +1,43 @@
 @extends('layouts.guest')
 @section('content')
     <section>
-        @foreach ($exam as $ex)
-            {{-- {{$ex->id}} --}}
-            <div class="col-md-4 card m-auto">
-                @foreach ($questions as $que)
+       <form action="" class="card">
+        @foreach ($exams as $ex)
+        <div class="col-md-4 my-2 card m-auto">
+            <h1 class="text-center"> {{$ex->title}} </h1>
 
-                <div class="jumbotron question">
+            @foreach ($questions as $que)
 
-                    {{$que->question}}
-                </div>
-                <div class="options">
-                    <ol>
-                       @foreach ($option as $opt)
-                       <li>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-
-                                <span class="input-group-text">
-                                    <input type="radio" name="answer" onclick="$('.question-answers').val(0);
-                                    $(this).next('.question-answers').val(1)"
-                                     class="" value="0">
-                                     <input type="hidden" value="0" class="question-answers form-control" name="answers[]">
-                                </span>
-                            </div>
-                            <input type="text" class="form-control option" name="options[]" value="{{$opt->options}}">
-                        </div>
-                    </li>
-                       @endforeach
-                    </ol>
-                </div>
-                @endforeach
+            <div class="jumbotron question">
+                <input type="" class="form-control" value="{{$ex->id}}">
+                <input type="" class="form-control" value="{{$user}}">
+                <input type="" class="form-control" value="{{$que->id}}">
+                {{$que->question}}
             </div>
-        @endforeach
+            <div class="options">
+                <ol>
+                   @foreach ($option as $opt)
+                   <li>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+
+                            <span class="input-group-text">
+                                <input type="radio" name="answer" onclick="$('.question-answers').val(0);
+                                $(this).next('.question-answers').val(1)"
+                                 class="" value="0">
+                                 <input type="hidden" value="0" class="question-answers form-control" name="answers[]">
+                            </span>
+                        </div>
+                        <input type="text" class="form-control option" name="options[]" value="{{$opt->options}}">
+                    </div>
+                </li>
+                   @endforeach
+                </ol>
+            </div>
+            @endforeach
+        </div>
+    @endforeach
+       </form>
 
     </section>
 @endsection
